206947ae01f2e68eb6c1eaea18783bb63301f225
Andrej Arsov <andrejarsovfax@gmail.com>
2024-05-16 21:07:25 +0200

    draft1

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import HeroBannerImage from '../assets/images/banner2.jpg';
+
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#2C8C99" fontWeight="600" fontSize="26px">Active Life</Typography>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+      Move, Energize <br />
+      And Repeat
+    </Typography>
+    <Typography fontSize="22px" fontFamily="Roboto" lineHeight="35px">
+      Discover invigorating exercises tailored for you
+    </Typography>
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#2C8C99', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
+    <Typography fontWeight={600} color="#2C8C99" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+      Energy
+    </Typography>
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+  </Box>
+);
+
+
+export default HeroBanner;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions= {
+    method: 'GET',
+    params: {limit: '10'},
+    headers: {
+      'X-RapidAPI-Key': 'b53f37ab55msh96ca8df4ef65f67p1b81e5jsn1a9c7c72ade8',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+
+
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    },
+  };
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+}
